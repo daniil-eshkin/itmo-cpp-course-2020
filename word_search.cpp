f11a7d84234cb5fccb4227421b50826971e1f283
daniil-eshkin <35163290+daniil-eshkin@users.noreply.github.com>
2020-03-21 16:11:10 +0300

    added word_search

--- word_search.cpp
+++ word_search.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 #include <cstring>
-
-const size_t BUFF_SIZE = 1024;
-
-FILE *file;
-char buff[BUFF_SIZE];
-size_t ptr, size;
-
-int get_char() {
-    if (ptr == size) {
-        size = fread(buff, sizeof(char), BUFF_SIZE, file);
-    }
-    if (size == 0) {
-        return -1;
-    } else {
-        return buff[ptr++];
-    }
-}
+#include <cstdio>
 
 size_t new_pi(size_t *pi, char *s, char symbol, size_t last_pi) {
     size_t i = last_pi;
@@ -29,10 +13,10 @@ size_t new_pi(size_t *pi, char *s, char symbol, size_t last_pi) {
     return i;
 }
 
-bool check_file(size_t *pi, char *s, size_t n) {
+bool check_file(FILE *file, size_t *pi, char *s, size_t n) {
     size_t last_pi = 0;
     int symbol;
-    while ((symbol = get_char()) != -1) {
+    while ((symbol = getc(file)) != EOF) {
         if ((last_pi = new_pi(pi, s, (char)symbol, last_pi)) == n) {
             return true;
         }
@@ -46,8 +30,8 @@ int main(int argc, char* argv[]) {
     for (size_t i = 1; i < n; i++) {
         pi[i] = new_pi(pi, argv[1], argv[1][i], pi[i - 1]);
     }
-    file = fopen(argv[2], "r");
-    std::cout << check_file(pi, argv[1], n) << std::endl;
+    FILE *file = fopen(argv[2], "r");
+    std::cout << (check_file(file, pi, argv[1], n)  ? "true" : "false") << std::endl;
     fclose(file);
 
     return 0;
